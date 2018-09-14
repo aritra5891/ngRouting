afdd73430dc0fa50a51940e1ef99fe38dbb0f765
Ganguly <492476@cognizant.com>
2018-09-14 18:02:50 +0530

    Routing Commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  loadServer() {
+    this.router.navigate(['/servers']);
+  }
+
+  loadServer1(id : number) {
+    this.router.navigate(['/servers', id, 'edit'], {queryParams : {ok: 'not', really: 'yes'}, fragment:'loading'});
   }
 
 }
